e19c33ba879755ec9f8e39e86a72c6d3aebe6ae5
vibhav trivedi <96198369+vibhavtrivediWEBDEV@users.noreply.github.com>
2022-03-10 17:59:12 +0530

    my portfolio design

---
+++ index.js
@@ -0,0 +1,124 @@
+$(document).ready(function () {
+    // $(window).on('load',function(){
+
+    // })
+
+
+
+    $(".hi").show(2000)
+
+
+
+
+
+
+    $('.img').show(3000),
+        $('.about h3').show(8000),
+        $('.about').show(6000)
+
+
+    $('.every').click(function () {
+        $(".one").attr("src", "https://media.istockphoto.com/photos/quality-control-certification-checked-guarantee-of-standard-picture-id1282804749?b=1&k=20&m=1282804749&s=170667a&w=0&h=pxajgIoOB8XGjTPHwWAsnVS3PCoUZWxBwCYBdpqCVk8=");
+
+    })
+    $('.webone').click(function () {
+        $(".one").attr("src", "https://media.istockphoto.com/photos/social-media-concept-picture-id1190110382?b=1&k=20&m=1190110382&s=170667a&w=0&h=kBbGXMT8-I8ksd-JLdcpz0hDCxCjNM8l0pW7Q_98vGg=");
+
+    })
+    $('.uione').click(function () {
+        $(".one").attr("src", "https://media.istockphoto.com/photos/digital-artist-working-at-home-picture-id1213781338?b=1&k=20&m=1213781338&s=170667a&w=0&h=bDVoUJdiVm7eA_Kw1yEcX1mS-PnB0Jh6bS00NNVnu-g=");
+
+    })
+    $('.brand').click(function () {
+        $(".one").attr("src", "https://media.istockphoto.com/photos/search-engine-optimization-concept-on-computer-screen-with-two-in-picture-id1300930633?b=1&k=20&m=1300930633&s=170667a&w=0&h=gOk_9UJb3Zb0yHtIk4WPxd3-Dnf-L-p4CLxlF6Ukq20=");
+
+    })
+
+
+    // two
+
+
+    $('.every').click(function () {
+        $(".two").attr("src", "https://images.unsplash.com/photo-1576595580361-90a855b84b20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZGVzaWduJTIwd2VifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60");
+
+    })
+    $('.webone').click(function () {
+        $(".two").attr("src", "https://media.istockphoto.com/photos/web-design-layout-internet-template-picture-id1127921873?b=1&k=20&m=1127921873&s=170667a&w=0&h=kTM4O96DBWH5PQlzAgo4TVA1bBdhki3PAOu8MfmUwT4=");
+
+    })
+    $('.uione').click(function () {
+        $(".two").attr("src", "https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGRlc2lnbiUyMHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60");
+
+    })
+    $('.brand').click(function () {
+        $(".two").attr("src", "https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRlc2lnbiUyMHdlYnNpdGVicmFuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60");
+
+    })
+
+    // three
+
+
+    $('.every').click(function () {
+        $('.every').css('color', 'white')
+        $(".three").attr("src", "https://images.unsplash.com/photo-1564424555153-04228f0aa7ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRlZ2lnbiUyMHdpcmVmcmFtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60");
+
+    })
+    $('.webone').click(function () {
+        $('.webone').css('color', 'white')
+        $(".three").attr("src", "https://media.istockphoto.com/photos/concept-of-mobile-apps-picture-id880805262?b=1&k=20&m=880805262&s=170667a&w=0&h=VHOxo3YTPQ6DWBoEX0TeyV38a_YPpmWwt4xDdgMgX14=");
+
+    })
+    $('.uione').click(function () {
+        $('.uione').css('color', 'white')
+        $(".three").attr("src", "https://media.istockphoto.com/photos/close-up-ux-developer-and-ui-designer-use-augmented-reality-app-picture-id1302423375?b=1&k=20&m=1302423375&s=170667a&w=0&h=mUHANZb3OVBVJpNjdX1LgiC0doJIyzegSrQoprVyuy4=");
+
+
+    })
+    $('.brand').click(function () {
+        $('.brand').css('color', 'white')
+        $(".three").attr("src", "https://media.istockphoto.com/photos/designers-drawing-website-ux-app-development-picture-id1196702694?b=1&k=20&m=1196702694&s=170667a&w=0&h=fYcxErTaRLhVWNkMeZ95FP0J6KM-J9seTTSyykRmn4w=");
+
+    })
+
+
+// 
+
+$('#ek').click(function(){
+    $(".testimg").attr("src", "https://media.istockphoto.com/photos/portrait-of-young-woman-smiling-picture-id1304581885?b=1&k=20&m=1304581885&s=170667a&w=0&h=B7KElxD35m6lKn6R7aeKb70Lyft_fpf6CkuXRdUyBzI=");
+    $('.para').html("i love his work and he is very polite consectetur adipisicing elit. Tempora eius nam non aspernatur necessita hi there her i did some well thing from you self"),
+    $('.paraname').html("Mahi  yu"),
+    $('.paracom').html("founder ibm")
+})
+
+$('#do').click(function(){
+    $(".testimg").attr("src", "https://media.istockphoto.com/photos/portrait-of-a-young-man-picture-id1318928248?k=20&m=1318928248&s=612x612&w=0&h=ZjdEWV61TU6Be7bvORywaqXvUvtMYU5nM7xHkK9CAj4=");
+    $('.para').html("he is very creative designer i see his workctetur adipisicing elit. Tempora eius nam non aspernatur necessita hi there her i did some well thing from you self"),
+    $('.paraname').html("jack Bayer"),
+    $('.paracom').html("project engineer") 
+})
+
+$('#teen').click(function(){
+    
+    $(".testimg").attr("src", "https://media-exp1.licdn.com/dms/image/C5603AQE3tQvOjUaOsA/profile-displayphoto-shrink_100_100/0/1632557555317?e=1652313600&v=beta&t=JmqtEPZSsqE5qBODIYT_npv7LtiJmyNS7OJFEVvCXYg");
+    $('.para').html("lorem istesh thi yser ectetur adipisicing elit. Tempora eius nam non aspernatur necessita hi there her i did some well thing from you self"),
+    $('.paraname').html("Ritesh bharadwaj"),
+    $('.paracom').html("project engineer")
+})
+
+// google search
+
+
+$('.first-section').hover(function(){
+    $('#querry').hide(1000)
+})
+$('.search').hover(function(){
+    $('#querry').show(1000)
+})
+
+
+
+
+
+
+
+})
